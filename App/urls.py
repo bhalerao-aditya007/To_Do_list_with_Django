64a876804831676a62c732787886c3885d0e74ea
bhalerao-aditya007 <adityabhalerao32@gmail.com>
2025-08-10 01:03:46 +0530

    final commit

--- App/urls.py
+++ App/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name = 'addTask'),
+    path('mark_as_done/<int:pk>', views.mark_as_done, name = 'done'),
+    path('mark_as_undone/<int:pk>', views.mark_as_undone, name = 'undone'),
+    path('edit_task<int:pk>', views.edit_task, name = 'edit'),
+    path('delete_task<int:pk>', views.del_task, name = 'delete'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from App.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
+    completed = Task.objects.filter(is_completed = True).order_by('-updated_at')
     context = {
         'tasks': tasks,
+        'completed':completed,
     }
     return render (request, 'home.html', context)
